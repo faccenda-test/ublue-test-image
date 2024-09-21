610b57f563cdb14bec8e4130aa31a650d780fd63
Mauro Faccenda <faccenda@gmail.com>
2024-09-21 17:30:16 +0200

    test dnf with other packages

--- 1password.sh
+++ 1password.sh
@@ -2,6 +2,8 @@
 
 set -ouex pipefail
 
-curl -Lo /tmp/1password-latest.rpm https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
 curl -Lo /tmp/1password-cli-latest.x86_64.rpm https://downloads.1password.com/linux/rpm/stable/x86_64/1password-cli-latest.x86_64.rpm
-rpm-ostree install /tmp/1password-latest.rpm /tmp/1password-cli-latest.x86_64.rpm
+curl -Lo /tmp/1password-latest.rpm https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
+
+rpm-ostree install /tmp/1password-cli-latest.x86_64.rpm
+rpm-ostree install /tmp/1password-latest.rpm
