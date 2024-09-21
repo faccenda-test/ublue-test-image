797bcc1df92b474b7cae208f59f5e2358a6cab8f
Mauro Faccenda <faccenda@gmail.com>
2024-09-21 16:21:36 +0200

    install 1password

---
+++ 1password.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+
+sudo dnf install 1password 1password-cli
