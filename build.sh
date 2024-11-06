3b400b6cfaf20895fcb0ee9578ecf77ba9d1e4e6
Mauro Faccenda <faccenda@gmail.com>
2024-11-06 14:38:31 +0100

    add 1password repo key

--- build.sh
+++ build.sh
@@ -13,12 +13,28 @@ RELEASE="$(rpm -E %fedora)"
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-curl -Lo /etc/pki/rpm-gpg/1password.asc https://downloads.1password.com/linux/keys/1password.asc
-mv /tmp/1password.repo /etc/yum.repos.d/1password.repo
+cat > /etc/yum.repos.d/1password.repo << EOF
+[1password]
+name=1Password Stable Channel
+baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+#gpgkey="https://downloads.1password.com/linux/keys/1password.asc"
+gpgkey=file:///etc/pki/rpm-gpg/1password.asc
+EOF
 
+curl -Lo /etc/pki/rpm-gpg/1password.asc https://downloads.1password.com/linux/keys/1password.asc
+
+curl -Lo /tmp/google-chrome-stable.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+rpm-ostree refresh-md
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-rpm-ostree install tmux 1password
+rpm-ostree install tmux /tmp/google-chrome-stable.rpm
+rpm-ostree status
+rpm-ostree upgrade
+
 
 systemctl enable podman.socket
 
